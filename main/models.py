896bece32ecb4fc0925b7a4110aeb9359ef95ca4
unknown <islombekahmadaliev10@gmail.com>
2025-02-10 15:00:03 +0500

    first commit

---
+++ main/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+class Correct(models.Model):
+    word = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.word
+class InCorrect(models.Model):
+    word = models.CharField(max_length=100)
+    correct = models.ForeignKey(Correct, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.word
